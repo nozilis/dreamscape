a1ba683e4346ce676fe641dec5d328dfc372ad8e
nozilis <kreonix06110611@gmail.com>
2026-04-19 00:46:42 +0300

    feat: install DRF and initial serializers

---
+++ dreamscape_app/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import User, Category, Wish
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'title']
+
+class WishSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Wish
+        fields = ['title', 'description', 'last_wish_at', 'images', 'is_done', 'location', 'gift_url', 'category']
