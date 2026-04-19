48f70676a167e411e658c1f9856d10d096935122
nozilis <kreonix06110611@gmail.com>
2026-04-19 11:13:16 +0300

    feat: APIView serializer class for Wish

--- dreamscape_app/views.py
+++ dreamscape_app/views.py
@@ -16,3 +16,10 @@ class WishListView(APIView):
         wishes = Wish.objects.filter(user=request.user).order_by('-created_at')
         serializer = WishSerializer(wishes, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = WishSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
