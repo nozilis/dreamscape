a1a0e625d445df492e965820f4dbf4e14f00f00d
nozilis <kreonix06110611@gmail.com>
2026-04-19 23:25:14 +0300

    refactor: convert APIView to ViewSet and update URL routing

--- dreamscape_app/views.py
+++ dreamscape_app/views.py
@@ -1,25 +1,20 @@
 from django.shortcuts import render
-
-from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet 
 from rest_framework.response import Response
 from .models import Category, Wish
 from .serializers import CategorySerializer, WishSerializer
 
-class CategoryListView(APIView):
-    def get(self, request):
-        categories = Category.objects.all()
-        serializer = CategorySerializer(categories, many=True)
-        return Response(serializer.data)
+class CategoryViewSet(ModelViewSet):
+    serializer_class = CategorySerializer
 
-class WishListView(APIView):
-    def get(self, request):
-        wishes = Wish.objects.filter(user=request.user).order_by('-created_at')
-        serializer = WishSerializer(wishes, many=True)
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = WishSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(user=request.user)
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+    def get_queryset(self):
+        return Category.objects.all()
+
+class WishViewSet(ModelViewSet):
+    serializer_class = WishSerializer
+
+    def get_queryset(self):
+        return Wish.objects.filter(user=self.request.user).order_by('-created_at')
+
+    def perform_create(self, serializer):
+        return serializer.save(user=self.request.user)
