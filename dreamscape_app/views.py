ac661fb80e4ea5b7aab164ff82aa56fc216470e0
nozilis <kreonix06110611@gmail.com>
2026-04-19 23:41:22 +0300

    fix: add permission classes to WishViewSet

--- dreamscape_app/views.py
+++ dreamscape_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet 
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .models import Category, Wish
 from .serializers import CategorySerializer, WishSerializer
 
@@ -10,6 +11,7 @@ class CategoryViewSet(ModelViewSet):
 
 class WishViewSet(ModelViewSet):
     serializer_class = WishSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return Wish.objects.filter(user=self.request.user).order_by('-created_at')
