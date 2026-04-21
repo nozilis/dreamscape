25bb2414272859997f55df4c5266ade0da8d275a
nozilis <kreonix06110611@gmail.com>
2026-04-21 21:23:49 +0300

    feat: connect custom permissions to WishViewSet

--- dreamscape_app/permissions.py
+++ dreamscape_app/permissions.py
@@ -1,9 +1,9 @@
 from rest_framework import permissions
 
 class UserAccessPermission(permissions.BasePermission):
-    message = 'Adding user not allowed.'
+    message = 'You do not have permission to modify this object.'
     
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.owner == request.user
+        return obj.user == request.user
