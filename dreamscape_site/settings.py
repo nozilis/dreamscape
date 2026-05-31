c8bec9a866d8cc15c3c45b15480b342e1c6eb07a
nozilis <n0zilis@yandex.ru>
2026-05-31 15:11:45 +0300

    ci: update actions versions

--- dreamscape_site/settings.py
+++ dreamscape_site/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from decouple import config
 from celery.schedules import crontab
-from dreamscape_app import tasks
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
